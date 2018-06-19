f69d09cadcf620256be2e0ad6e85f83ef0cbd0da
liranzxc <lirannh@gmail.com>
2018-06-19 14:57:48 +0300

    first commit

---
+++ personify/js/main.js
@@ -0,0 +1,294 @@
+(function($) {
+
+	'use strict';
+
+	// bootstrap dropdown hover
+
+  // loader
+  var loader = function() {
+    setTimeout(function() { 
+      if($('#loader').length > 0) {
+        $('#loader').removeClass('show');
+      }
+    }, 1);
+  };
+  loader();
+
+	
+	$('nav .dropdown').hover(function(){
+		var $this = $(this);
+		$this.addClass('show');
+		$this.find('> a').attr('aria-expanded', true);
+		$this.find('.dropdown-menu').addClass('show');
+	}, function(){
+		var $this = $(this);
+			$this.removeClass('show');
+			$this.find('> a').attr('aria-expanded', false);
+			$this.find('.dropdown-menu').removeClass('show');
+	});
+
+
+  var offcanvas_toggle = $('.js-offcanvas-toggle');
+  offcanvas_toggle.on('click', function() {
+
+
+    if ( $('body').hasClass('offcanvas-open') ) {
+      $('body').removeClass('offcanvas-open');
+    } else {
+      $('body').addClass('offcanvas-open');
+    }
+
+  });
+
+
+  ////
+//update this with your js_form selector
+var form_id_js = "javascript_form";
+
+var data_js = {
+    "access_token": "wm43zus0jcc7curwmxs2c64k"
+};
+
+function js_onSuccess() {
+    // remove this to avoid redirect
+    window.location = window.location.pathname + "?message=Email+Successfully+Sent%21&isError=0";
+}
+
+function js_onError(error) {
+    // remove this to avoid redirect
+    window.location = window.location.pathname + "?message=Email+could+not+be+sent.&isError=1";
+}
+
+var sendButton = document.getElementById("js_send");
+
+function js_send() {
+    sendButton.value='Sending…';
+    sendButton.disabled=true;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            js_onSuccess();
+        } else
+        if(request.readyState == 4) {
+            js_onError(request.response);
+        }
+    };
+
+    var subject = document.querySelector("#" + form_id_js + " [name='subject']").value;
+    var message = document.querySelector("#" + form_id_js + " [name='text']").value;
+    data_js['subject'] = subject;
+    data_js['text'] = message;
+    var params = toParams(data_js);
+
+    request.open("POST", "https://postmail.invotes.com/send", true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    request.send(params);
+
+    return false;
+}
+
+sendButton.onclick = js_send;
+
+function toParams(data_js) {
+    var form_data = [];
+    for ( var key in data_js ) {
+        form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key]));
+    }
+
+    return form_data.join("&");
+}
+
+var js_form = document.getElementById(form_id_js);
+js_form.addEventListener("submit", function (e) {
+    e.preventDefault();
+});
+
+
+
+
+
+
+
+  ////
+
+
+
+
+
+
+
+  $(document).click(function(e) {
+    var container = $('.js-offcanvas-toggle, #offcanvas_menu');
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ( $('body').hasClass('offcanvas-open') ) {
+        $('body').removeClass('offcanvas-open');
+      }
+    }
+  });
+
+
+  $('#date-countdown').countdown('2020/10/10', function(event) {
+  var $this = $(this).html(event.strftime(''
+    + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
+    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
+    + '<span class="countdown-block"><span class="label">%H</span> hr </span>'
+    + '<span class="countdown-block"><span class="label">%M</span> min </span>'
+    + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
+});
+
+	// home slider
+	$('.home-slider').owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin:10,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav:true,
+    autoplayHoverPause: false,
+    items: 1,
+    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
+    responsive:{
+      0:{
+        items:1,
+        nav:false
+      },
+      600:{
+        items:1,
+        nav:false
+      },
+      1000:{
+        items:1,
+        nav:true
+      }
+    }
+	});
+
+
+  // home slider
+  $('.testimony-slider').owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin:10,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav:false,
+    autoplayHoverPause: false,
+    items: 1,
+    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
+    responsive:{
+      0:{
+        items:1,
+        nav:false
+      },
+      600:{
+        items:1,
+        nav:false
+      },
+      1000:{
+        items:1,
+        nav:false
+      }
+    }
+  });
+
+	// owl carousel
+	var majorCarousel = $('.js-carousel-1');
+	majorCarousel.owlCarousel({
+    loop:true,
+    autoplay: true,
+    stagePadding: 7,
+    margin: 20,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav: true,
+    autoplayHoverPause: true,
+    items: 3,
+    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
+    responsive:{
+      0:{
+        items:1,
+        nav:false
+      },
+      600:{
+        items:2,
+        nav:false
+      },
+      1000:{
+        items:3,
+        nav:true,
+        loop:false
+      }
+  	}
+	});
+
+	// owl carousel
+	var major2Carousel = $('.js-carousel-2');
+	major2Carousel.owlCarousel({
+    loop:true,
+    autoplay: true,
+    stagePadding: 7,
+    margin: 20,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav: true,
+    autoplayHoverPause: true,
+    items: 4,
+    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
+    responsive:{
+      0:{
+        items:1,
+        nav:false
+      },
+      600:{
+        items:3,
+        nav:false
+      },
+      1000:{
+        items:4,
+        nav:true,
+        loop:false
+      }
+  	}
+	});
+
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.element-animate').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('element-animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .element-animate.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							var effect = el.data('animate-effect');
+							if ( effect === 'fadeIn') {
+								el.addClass('fadeIn element-animated');
+							} else if ( effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft element-animated');
+							} else if ( effect === 'fadeInRight') {
+								el.addClass('fadeInRight element-animated');
+							} else {
+								el.addClass('fadeInUp element-animated');
+							}
+							el.removeClass('item-animate');
+						},  k * 100);
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '95%' } );
+	};
+	contentWayPoint();
+
+
+
+})(jQuery);
